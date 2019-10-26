9f3530f674e1d549926015bc93ab903cc593abbf
sugiyamatoshiki <s.toshiki.llwz8869@gmail.com>
2019-10-26 20:27:30 +0900

    edit taskController

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Folder;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
     public function index()
     {
-        return 'Hello world';
+        $folders = Folder::all();
+        return view('task/index', [
+            'folders' => $folders,
+        ]);
     }
 }
