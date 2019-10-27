39db81c90ee29fbc0d35733021406d713ed57f14
sugiyamatoshiki <s.toshiki.llwz8869@gmail.com>
2019-10-27 20:01:38 +0900

    show label of tasks

--- app/Task.php
+++ app/Task.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    //
+    const STATUS = [
+        1 => ['label' => '未着手'],
+        2 => ['label' => '進行中'],
+        3 => ['label' => '完了'],
+    ];
+
+    public function getStatusLabelAttribute()
+    {
+        $status = $this->attributes['status'];
+        if (!isset(self::STATUS[$status])) {
+            return '';
+        }
+        return self::STATUS[$status]['label'];
+    }
 }
