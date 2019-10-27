78f3f0b0406f253f81f2bcb56248143820ce59e6
sugiyamatoshiki <s.toshiki.llwz8869@gmail.com>
2019-10-27 12:58:44 +0900

    create tasks table

---
+++ database/migrations/2019_10_27_124002_create_tasks_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('folder_id')->unsigned();
+            $table->string('title', 100);
+            $table->integer('status')->default(1);
+            $table->date('due_date');
+            $table->timestamps();
+
+            $table->foreign('folder_id')->references('id')->on('folders')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
