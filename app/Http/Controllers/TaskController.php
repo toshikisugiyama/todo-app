520b731159cb357f52929ea43a75d04537c42dfa
sugiyamatoshiki <s.toshiki.llwz8869@gmail.com>
2019-10-27 15:18:08 +0900

    edit task controller

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Task;
 use App\Folder;
 use Illuminate\Http\Request;
 
@@ -10,9 +11,12 @@ class TaskController extends Controller
     public function index(int $id)
     {
         $folders = Folder::all();
+        $current_folder = Folder::find($id);
+        $tasks = Task::where('folder_id', $current_folder->id)->get();
         return view('tasks/index', [
             'folders' => $folders,
             'current_folder_id' => $id,
+            'tasks' => $tasks,
         ]);
     }
 }
