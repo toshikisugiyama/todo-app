8ac18142323fa3e5a51dedd246609c7e135bcc2f
sugiyamatoshiki <s.toshiki.llwz8869@gmail.com>
2019-10-27 21:00:57 +0900

    relation of tasks and folders table

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -12,7 +12,7 @@ class TaskController extends Controller
     {
         $folders = Folder::all();
         $current_folder = Folder::find($id);
-        $tasks = Task::where('folder_id', $current_folder->id)->get();
+        $tasks = $current_folder->tasks()->get();
         return view('tasks/index', [
             'folders' => $folders,
             'current_folder_id' => $id,
