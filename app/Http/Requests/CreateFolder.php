64ea9d0bafc06d7bf709576b7f50fbff2e2bdae0
sugiyamatoshiki <s.toshiki.llwz8869@gmail.com>
2019-10-28 10:38:23 +0900

    make folder title validation in Japanese

--- app/Http/Requests/CreateFolder.php
+++ app/Http/Requests/CreateFolder.php
@@ -6,6 +6,14 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class CreateFolder extends FormRequest
 {
+
+    public function attributes()
+    {
+        return [
+            'title' => 'フォルダ名',
+        ];
+    }
+
     /**
      * Determine if the user is authorized to make this request.
      *
