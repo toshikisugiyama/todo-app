dc80ed3cf1f05169fcf59d3fcf6cd37aa71ff78e
sugiyamatoshiki <s.toshiki.llwz8869@gmail.com>
2019-10-31 15:57:55 +0900

    change header log in and log out

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -12,6 +12,18 @@
 <header>
   <nav class="my-navbar">
     <a href="/" class="my-navbar-brand">ToDo App</a>
+    <div class="my-navbar-control">
+      @if(Auth::check())
+        <span class="my-navbar-item">ようこそ、{{ Auth::user()->name }}さん</span>
+        |
+        <a href="#" id="logout" class="my-navbar-item">ログアウト</a>
+        <form id="logout-form"  action="{{ route('logout') }}" method="post" style="desplay: none;"></form>
+      @else
+        <a class="my-navbar-item" href="{{ route('login') }}">ログイン</a>
+        |
+        <a class="my-navbar-item" href="{{ route('register') }}">会員登録</a>
+      @endif
+    </div>
   </nav>
 </header>
 <main>
