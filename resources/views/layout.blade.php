8817adf6fe43da316fdb610399eecbfb2908659c
sugiyamatoshiki <s.toshiki.llwz8869@gmail.com>
2019-10-31 16:08:58 +0900

    add script for logout pagenation in layout

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -29,6 +29,14 @@
 <main>
   @yield('content')
 </main>
+@if(Auth::check())
+  <script>
+    document.getElementById('logout').addEventListener('click', event => {
+      event.preventDefault();
+      document.getElementById('logout-form').submit();
+    });
+  </script>
+@endif
 @yield('scripts')
 </body>
 </html>
