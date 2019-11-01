a7376b8ed4e8d2988bc931c9bdacedaf5a537e08
sugiyamatoshiki <s.toshiki.llwz8869@gmail.com>
2019-11-01 18:07:06 +0900

    add TaskController, and Task model, and made layout template, and edit route

---
+++ resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layout')
+
+@section('content')
+  <div class="container">
+    <div class="row">
+      Home
+    </div>
+  </div>
+@endsection
---
+++ resources/views/tasks/index.blade.php
@@ -0,0 +1,24 @@
+@extends('layout')
+
+@section('content')
+  <div class="container">
+    <div class="row">
+      <div class="panel-group col col-md-4">
+        <div class="panel panel-default">
+          <div class="panel-heading">フォルダ</div>
+          <div class="panel-body">
+            <a href="#">
+              フォルダを追加する
+            </a>
+            <div class="list-group">
+              <a href="" class="list-group-item"></a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col col-md-8">
+
+      </div>
+    </div>
+  </div>
+@endsection
