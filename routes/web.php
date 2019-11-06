437ec062a60208e5a459006f01dfb8250d0c8671
sugiyamatoshiki <s.toshiki.llwz8869@gmail.com>
2019-11-06 22:14:57 +0900

    add middleware in routes/web

--- routes/web.php
+++ routes/web.php
@@ -11,16 +11,17 @@
 |
 */
 
-
-Route::get('folders/{id}/tasks', 'TaskController@index')->name('tasks.index');
-Route::get('/folders/create', 'FolderController@create')->name('folders.create');
-Route::post('/folders/create', 'FolderController@store');
-Route::get('/folders/{id}/tasks/create', 'TaskController@create')->name('tasks.create');
-Route::post('/folders/{id}/tasks/create', 'TaskController@store');
-Route::get('/folders/{id}/tasks/{task_id}/edit', 'TaskController@edit')->name('tasks.edit');
-Route::post('/folders/{id}/tasks/{task_id}/edit', 'TaskController@update');
-Route::get('/folders/{id}/tasks/{task_id}/delete', 'TaskController@destroy')->name('tasks.delete');
+Route::group(['middleware' => 'auth'], function(){
+    Route::get('/', 'HomeController@index')->name('home');
+    Route::get('folders/{id}/tasks', 'TaskController@index')->name('tasks.index');
+    Route::get('/folders/create', 'FolderController@create')->name('folders.create');
+    Route::post('/folders/create', 'FolderController@store');
+    Route::get('/folders/{id}/tasks/create', 'TaskController@create')->name('tasks.create');
+    Route::post('/folders/{id}/tasks/create', 'TaskController@store');
+    Route::get('/folders/{id}/tasks/{task_id}/edit', 'TaskController@edit')->name('tasks.edit');
+    Route::post('/folders/{id}/tasks/{task_id}/edit', 'TaskController@update');
+    Route::get('/folders/{id}/tasks/{task_id}/delete', 'TaskController@destroy')->name('tasks.delete');
+});
 
 Auth::routes();
 
-Route::get('/', 'HomeController@index')->name('home');
